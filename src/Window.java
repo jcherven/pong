3a9125ee470fc3f2ffd4ee2a06fdd6129cf91249
jcherven <jamiecherven@gmail.com>
2020-11-29 15:17:56 -0500

    adds basic user input listener

--- src/Window.java
+++ src/Window.java
@@ -1,10 +1,12 @@
 import javax.swing.JFrame;
 import java.awt.Graphics2D;
 import java.awt.Color;
+import java.awt.event.KeyEvent;
 
 public class Window extends JFrame implements Runnable {
 
 	Graphics2D g2;
+	KL keyListener = new KL();
 
 	public Window() {
 		this.setSize(Constants.SCREEN_WIDTH,Constants.SCREEN_HEIGHT);
@@ -12,15 +14,24 @@ public class Window extends JFrame implements Runnable {
 		this.setResizable(false);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.addKeyListener(keyListener);
 		g2 = (Graphics2D)this.getGraphics();
 	}
 	
 	public void update(double dt) {
-		System.out.println("" + dt + " seconds have passed since the last frame");
 		System.out.println(1 / dt + "fps");
 		
 		g2.setColor(Color.BLACK);
 		g2.fillRect( 0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT );
+		
+		// user input
+		if (keyListener.isKeyPressed(KeyEvent.VK_UP)) {
+			System.out.println("user is pressing UP arrow");
+		}
+		
+		if (keyListener.isKeyPressed(KeyEvent.VK_DOWN)) {
+			System.out.println("user is pressing DOWN arrow");
+		}
 	}
 
 	// game loop
